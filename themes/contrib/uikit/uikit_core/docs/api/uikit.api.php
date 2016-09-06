dd40379efb4225578ae7de45fd7470c24b703e94
Richard Buchanan <richard_buchanan@buchanandesigngroup.com>
2016-09-05 20:01:27 -0400

    Added documentation to the theme settings API topic.

--- themes/contrib/uikit/uikit_core/docs/api/uikit.api.php
+++ themes/contrib/uikit/uikit_core/docs/api/uikit.api.php
@@ -82,23 +82,50 @@
  * @lead settings Customizing UIkit from the Drupal administration back-end.
  * UIkit comes with an extensive variety of theme settings to configure almost
  * all themeable aspects of your Drupal site. This topic provides a brief
- * overview of these theme settings.
+ * overview of these theme settings to customize the look of your website.
+ *
+ * @subtitle Jump to a section
+ * - @ref theme_styles
+ * - @ref mobile_settings
+ * - @ref layout
+ * - @ref navigations
+ * - @ref elements
+ * - @ref common
+ * - @ref javascript
+ * - @ref components
  *
  * @section theme_styles Theme styles
+ * UIkit comes with a basic theme and two neat themes to get you started. Here
+ * you can select which base style to start with.
  *
  * @section mobile_settings Mobile settings
+ * Adjust the mobile layout settings to enhance your users' experience on
+ * smaller devices.
  *
  * @section layout Layout
+ * Apply our fully responsive fluid grid system and panels, common layout parts
+ * like blog articles and comments and useful utility classes.
  *
  * @section navigations Navigations
+ * UIkit offers different types of navigations, like navigation bars and side
+ * navigations. Use breadcrumbs or a pagination to steer through articles.
  *
  * @section elements Elements
+ * Style basic HTML elements, like tables and forms. These components use their
+ * own classes. They won't interfere with any default element styling.
  *
  * @section common Common
+ * Here you'll find components that you often use within your content, like
+ * buttons, icons, badges, overlays, animations and much more.
  *
  * @section javascript Javascript
+ * These components rely mostly on JavaScript to fade in hidden content, like
+ * dropdowns, modal dialogs, off-canvas bars and tooltips.
  *
  * @section components Components
+ * UIkit offers some advanced components that are not included in the UIkit core
+ * framework. Usually you wouldn't use these components in your everyday
+ * website.
  * @}
  */
 
