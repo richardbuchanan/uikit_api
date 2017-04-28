4455ab448c40bb79e241dd45737a7bab411b6fb4
Richard Buchanan <richard_buchanan@buchanandesigngroup.com>
2017-04-28 14:17:59 -0400

    Add modernizr module as dependency

--- themes/custom/docs/js/docs.js
+++ themes/custom/docs/js/docs.js
@@ -93,16 +93,18 @@
 
   Drupal.behaviors.externalLinks = {
     attach: function () {
-      $('a').each(function() {
-        if (this.hostname !== location.host) {
-          // Make sure all external links open in a new tab.
-          $(this).prop('target', '_blank');
+      if (location.hostname === 'uikit-drupal.com') {
+        $('a').each(function () {
+          if (this.hostname !== location.host) {
+            // Make sure all external links open in a new tab.
+            $(this).prop('target', '_blank');
 
-          if (/drupal/i.test(this.hostname)) {
-            $(this).addClass('drupal-api');
+            if (/drupal/i.test(this.hostname)) {
+              $(this).addClass('drupal-api');
+            }
           }
-        }
-      });
+        });
+      }
     }
   };
 
