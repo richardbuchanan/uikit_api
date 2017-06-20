fa093af255fd98143879b97dddf55e6154927edd
Richard Buchanan <richardbuchanan@richardbuchanan.com>
2017-06-20 06:56:50 -0400

    Updated various aspects in forum posts

--- themes/custom/docs/templates/advanced_forum/advanced-forum-post-edited.tpl.php
+++ themes/custom/docs/templates/advanced_forum/advanced-forum-post-edited.tpl.php
@@ -6,8 +6,10 @@
  */
 ?>
 
-<span class="post-edit-label"><?php print t('Edited by'); ?>:</span> <?php print $edited_name; ?>
-<span class="post-edit-label"> <?php print t('on'); ?> </span> <?php print $edited_datetime; ?>
-<?php if (!empty($edited_reason)) : ?>
-  <span class="post-edit-label"> <?php print t('Reason'); ?>:</span> <?php print $edited_reason; ?>
-<?php endif; ?>
+<div class="uk-margin-top uk-margin-bottom">
+  <span class="post-edit-label"><?php print t('Edited by'); ?>:</span> <?php print $edited_name; ?>
+  <span class="post-edit-label"> <?php print t('on'); ?> </span> <?php print $edited_datetime; ?>
+  <?php if (!empty($edited_reason)) : ?>
+    <span class="post-edit-label"> <?php print t('Reason'); ?>:</span> <?php print $edited_reason; ?>
+  <?php endif; ?>
+</div>
